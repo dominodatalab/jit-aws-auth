1359dfe700c607208ead8cf8a16a8dea93487181
Marc Doan (Domino) <marc.doan@dominodatalab.com>
2025-10-01 10:10:12 -0400

    WIP: updating client logic to get groups first and do initial cred fetch by group

--- jit-client/jit-client-proxy.py
+++ jit-client/jit-client-proxy.py
@@ -99,6 +99,20 @@ def read_credentials_file(cred_file_path) -> list[dict]:
     return config_dict
 
 @backoff.on_exception(backoff.expo,requests.exceptions.RequestException,max_time=request_timeout,raise_on_giveup=False)
+def get_user_projects(user_jwt:str) -> list[str]:
+    headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + user_jwt,
+    }
+    request_endpoint = f'{service_endpoint}/user-projects'
+    logger.info(f'Fetching user projects from {request_endpoint}')
+    resp = requests.get(request_endpoint, headers=headers)
+    logger.info(f'User groups from {request_endpoint}: {resp.json()}')
+    resp.raise_for_status()
+    if resp.status_code == 200:
+        return list(resp.json())
+
+@backoff.on_exception(backoff.expo,requests.exceptions.RequestException,max_time=request_timeout,raise_on_giveup=False)
 def get_domino_user_identity():
     token = None
     token_endpoint = os.environ.get('DOMINO_API_PROXY','http://localhost:8899')
@@ -128,7 +142,7 @@ def check_credential_expiration(credential_list:list[dict]) -> list[dict]:
     return expiring_creds
 
 @backoff.on_exception(backoff.expo,requests.exceptions.RequestException,max_time=request_timeout,raise_on_giveup=False)
-def refresh_jit_credentials(project=None):
+def refresh_jit_credentials(project=None, jwt=None) -> list[dict]:
     # The structure we're expecting from the JIT Proxy:
     # [ 
     #     {
@@ -144,7 +158,10 @@ def refresh_jit_credentials(project=None):
         url = f'{service_endpoint}/{project}'
     else:
         url = service_endpoint
-    user_jwt = get_domino_user_identity()
+    if not jwt:
+        user_jwt = get_domino_user_identity()
+    else: 
+        user_jwt = jwt
     if user_jwt != None:
         creds = []
         headers = {
@@ -186,10 +203,14 @@ if __name__ == "__main__":
                 else:
                     logger.info("Attempted to refresh credentials, but response from JIT Proxy was empty. Will retry on next cycle.")
         else:
-            new_creds = refresh_jit_credentials()
-            if new_creds != None and len(new_creds) > 0:
-                write_credentials_profile(aws_credentials=new_creds,cred_file_path=aws_credentials_profile)
-                write_credentials_file(aws_credentials=new_creds,cred_file_path=aws_credentials_file)
+            user_jwt = get_domino_user_identity()
+            if user_jwt:
+                user_projects = get_user_projects(user_jwt)
+                for project in user_projects:
+                    new_creds = refresh_jit_credentials(project=project, jwt=user_jwt)
+                    if new_creds != None and len(new_creds) > 0:
+                        write_credentials_profile(aws_credentials=new_creds,cred_file_path=aws_credentials_profile)
+                        write_credentials_file(aws_credentials=new_creds,cred_file_path=aws_credentials_file)
         if not shutdown.shutdown_signal:
             logger.debug(f"Sleeping {poll_jit_interval} seconds until next attempt...")
             time.sleep(poll_jit_interval)
---
+++ jit-proxy-server/gunicorn.conf.py
@@ -0,0 +1,39 @@
+import os
+from psycogreen.gevent import patch_psycopg
+from prometheus_flask_exporter.multiprocess import GunicornPrometheusMetrics
+from os import getenv, makedirs
+from os.path import exists
+
+bind_addr = os.getenv('FLASK_HOST','0.0.0.0')
+bind_port = os.getenv('FLASK_PORT','6000')
+worker_count = os.getenv('FLASK_WORKERS',1)
+
+bind = f"{bind_addr}:{bind_port}"
+workers = worker_count
+certfile = "/ssl/tls.crt"
+keyfile = "/ssl/tls.key"
+reload_extra_files = "/ssl/tls.crt"
+
+# patch forked gevent processes for psycopg2
+def post_fork(server, worker):
+    # gevent.monkey.patch_all() is already called in GEventWorker for us
+    worker.log.info("patching psycopg2 with psycogreen")
+    patch_psycopg()
+
+    worker.log.info("patching complete")
+
+
+# Prometheus metrics config
+def when_ready(server):
+
+    prom_multip_path = getenv("PROMETHEUS_MULTIPROC_DIR", "/pi/metrics")
+    if not exists(prom_multip_path):
+        makedirs(prom_multip_path)
+
+    GunicornPrometheusMetrics.start_http_server_when_ready(
+        int(getenv("PROMETHEUS_METRICS_PORT", "8080"))
+    )
+
+
+def child_exit(server, worker):
+    GunicornPrometheusMetrics.mark_process_dead_on_child_exit(worker.pid)
